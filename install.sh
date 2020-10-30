d16f8e263ecdca1128d2440ede20fb82e5bc9dd1
Peter van der Velde <petervandervelde2@gmail.com>
2020-10-30 13:30:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 mkdir -pv ~/.local/share/gtksourceview-3.0/styles/
 cp -v ./*.xml ~/.local/share/gtksourceview-3.0/styles/
 
-mkdir -pv ~/.local/share/gtksourceview-3.0/styles/
+mkdir -pv ~/.local/share/gtksourceview-4/styles/
 cp -v ./*.xml ~/.local/share/gtksourceview-4/styles/
